3ae143ba257d5f9b5b2e940a381423af9f9cb973
Eric Yun <yunxiuyu@hotmail.com>
2022-01-25 11:58:52 -0700

    70

--- Dynamic Programming/70-climbing-stairs/main.py
+++ Dynamic Programming/70-climbing-stairs/main.py
@@ -1,41 +1,28 @@
+# class Solution:
 class Solution:
+    # solution3: dynamic programing - bottom-up
+    #   base cases: dp[1] = 1, dp[2]=2
+    #   solve problem when n stairs
+    #   for i in range(n+1):
+    #       dp[n] = dp[n-2] + dp[n-1]   # there are two ways can climb to n which is n-1 and n-2
+
     def climbStairs(self, n: int) -> int:
-        memo = {1: 1, 2: 2}
-
-        def climb(n):
-            if n in memo:
-                return memo[n]
-            memo[n] = climb(n - 1) + climb(n - 2)
-            return memo[n]
-
-        return climb(n)
-
+        if n in (1, 2):
+            return n
+        dp = [None] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
 
 sln = Solution()
+
 assert sln.climbStairs(2) == 2, "expect 2"
-assert sln.climbStairs(3) == 3, f"{sln.climbStairs(3)}, expect 3"
+assert sln.climbStairs(3) == 3, "expect 3"
 assert sln.climbStairs(5) == 8, "expect 8"
 
-# # solution3: dynamic programing
-# class Solution:
-#     def climbStairs(self, n: int) -> int:
-#         if n in (1, 2):
-#             return n
-#         dp = [None] * (n + 1)
-#         dp[1] = 1
-#         dp[2] = 2
-#         for i in range(3, n + 1):
-#             dp[i] = dp[i - 1] + dp[i - 2]
-#         return dp[n]
-
-
-# sln = Solution()
-# assert sln.climbStairs(2) == 2, "expect 2"
-# assert sln.climbStairs(3) == 3, f"{sln.climbStairs(3)}, expect 3"
-# assert sln.climbStairs(5) == 8, "expect 8"
-
-
-# solution2, recursion + memoize
+# solution2, recursion + memoize (list)
 # class Solution:
 #     def climbStairs(self, n: int) -> int:
 #         memo = [None] * (n + 1)
@@ -54,12 +41,18 @@ assert sln.climbStairs(5) == 8, "expect 8"
 #         print(f"{memo}")
 #         return memo[i]
 
+# solution2.5, recursion + memoization (dict)
+# class Solution:
+# def climbStairs(self, n: int) -> int:
+#     memo = {1: 1, 2: 2}
 
-# sln = Solution()
+#     def climb(n):
+#         if n in memo:
+#             return memo[n]
+#         memo[n] = climb(n - 1) + climb(n - 2)
+#         return memo[n]
 
-# assert sln.climbStairs(2) == 2, "expect 2"
-# assert sln.climbStairs(3) == 3, "expect 3"
-# assert sln.climbStairs(5) == 8, "expect 8"
+#     return climb(n)
 
 
 # solution1: recursion.
@@ -74,9 +67,3 @@ assert sln.climbStairs(5) == 8, "expect 8"
 #             return 0
 #         return self.climb(i + 1, n) + self.climb(i + 2, n)
 
-
-# sln = Solution()
-
-# assert sln.climbStairs(2) == 2, "expect 2"
-# assert sln.climbStairs(3) == 3, "expect 3"
-# assert sln.climbStairs(5) == 8, "expect 8"
