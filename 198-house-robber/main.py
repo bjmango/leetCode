e590a1e8c425d44b74d85aa126db72399134cf13
Eric Yun <yunxiuyu@hotmail.com>
2022-01-27 17:26:56 -0700

    add solution1

--- 198-house-robber/main.py
+++ 198-house-robber/main.py
@@ -11,6 +11,24 @@ class Solution:
             rob2 = temp
         return rob2
 
+    # solution 1:
+    # def __init__(self) -> None:
+    #     self.memo = {}
+
+    # def rob(self, nums: List[int]) -> int:
+    #     return self.helper(0, nums)
+
+    # def helper(self, i, nums):
+    #     if i >= len(nums):
+    #         return 0
+    #     if i in self.memo:
+    #         return self.memo[i]
+    #     # choose i or not choose i
+    #     ans = max(self.helper(i + 1, nums), self.helper(i + 2, nums) + nums[i])
+
+    #     self.memo[i] = ans
+    #     return ans
+
 
 sln = Solution()
 print(sln.rob([2, 7, 9, 3, 1]))
